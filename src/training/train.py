492c9e351a30a9fad69e6eaf4ceb16b70a215056
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-03-10 14:52:38 +0900

    model
    test succes

--- src/training/train.py
+++ src/training/train.py
@@ -7,34 +7,36 @@ def training_MT(tr_set,val_set,model,epochs,loss_fn,optimizer, output_path,batch
     dataloader = DataLoader(tr_set, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
     for epoch in range(epochs):
+        model.train()
+        total_loss = 0
         for x_batch, y_batch in dataloader:
-            model.train()
             torch.autograd.set_detect_anomaly(True)
 
             outputs = model(x_batch)
-
+            #print(outputs)
             # 各出力ごとの MSE を計算
             individual_losses = [loss_fn(output, target) for output, target in zip(outputs, y_batch)]
 
             # 合計損失
             train_loss = sum(individual_losses)
-
             train_loss.backward()
             optimizer.step()
+            total_loss += train_loss.item()
 
-            # モデルを評価モードに設定（検証データ用）
-            model.eval()
-            with torch.no_grad():
-                for x_batch, y_batch in val_dataloader:
-                    outputs_val = model(x_batch)
-                    # 各出力ごとの MSE を計算
-                    individual_losses_val = [loss_fn(output, target) for output, target in zip(outputs_val, y_batch)]
+        # モデルを評価モードに設定（検証データ用）
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for x_batch, y_batch in val_dataloader:
+                outputs_val = model(x_batch)
+                # 各出力ごとの MSE を計算
+                individual_losses_val = [loss_fn(output, target) for output, target in zip(outputs_val, y_batch)]
 
-                    # 合計損失
-                    val_loss = sum(individual_losses_val)
-                    print(f"Epoch [{epoch+1}/{epochs}], "
-                        f"Train Loss: {train_loss.item():.4f}, "
-                        f"Validation Loss: {val_loss.item():.4f}"
-                        )
+                # 合計損失
+                val_loss += sum(individual_losses_val).item()
+        print(f"Epoch [{epoch+1}/{epochs}], "
+            f"Train Loss: {train_loss:.4f}, "
+            f"Validation Loss: {val_loss:.4f}"
+            )
     return model
 
