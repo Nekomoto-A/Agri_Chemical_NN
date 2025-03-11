82b594400409f72b74a63f846840f0fb51fcbc7b
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-03-11 15:36:10 +0900

    model
    resucces

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -4,17 +4,22 @@ import pandas as pd
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,PowerTransformer
 from torch.utils.data import TensorDataset, dataloader
 
 class data_create:
-    def __init__(self,path_asv,path_chem):
+    def __init__(self,path_asv,path_chem,reg_list):
         self.asv_data = pd.read_csv(path_asv).drop('index',axis = 1)
         self.chem_data = pd.read_excel(path_chem)
-
+        self.reg_list = reg_list
     def __iter__(self):
-        yield self.asv_data
-        yield self.chem_data
+        for r in self.reg_list:
+            ind = self.chem_data[self.chem_data[r].isna()].index
+            asv_data = self.asv_data.drop(ind)
+            chem_data = self.chem_data.drop(ind)
+
+        yield asv_data
+        yield chem_data
 
 def clr_transform(data, geometric_mean=None,adjust = 1e-10):
     """
@@ -48,32 +53,31 @@ def transform_after_split(x_train,x_test,y_train,y_test,reg_list,val_size = 0.2)
     x_val_clr = x_val_clr.to_numpy()
     x_test_clr = x_test_clr.to_numpy()
 
-    pp = StandardScaler()
-    y_train_split_pp = pp.fit_transform(y_train_split[reg_list].to_numpy())
-    y_val_pp = pp.transform(y_val[reg_list].to_numpy())
-    y_test_pp = pp.transform(y_test[reg_list].to_numpy())
-
     X_train_tensor = torch.tensor(x_train_split_clr, dtype=torch.float32)
     X_val_tensor = torch.tensor(x_val_clr, dtype=torch.float32)
     X_test_tensor = torch.tensor(x_test_clr, dtype=torch.float32)
 
-    Y_train_tensor = torch.tensor(y_train_split_pp, dtype=torch.float32)
-    Y_val_tensor = torch.tensor(y_val_pp, dtype=torch.float32)
-    Y_test_tensor = torch.tensor(y_test_pp, dtype=torch.float32)
+    scalers = {}
+    Y_train_tensor, Y_val_tensor, Y_test_tensor = [], [], []
 
-    train_set = TensorDataset(X_train_tensor, Y_train_tensor)
-    val_set = TensorDataset(X_val_tensor, Y_val_tensor)
-    test_set = TensorDataset(X_test_tensor, Y_test_tensor)
+    for reg in reg_list:
+        pp = StandardScaler()
+        #pp = MinMaxScaler()
+        #pp = PowerTransformer(method='yeo-johnson')
 
-    return train_set,val_set,test_set
+        pp = pp.fit(y_train_split[reg].values.reshape(-1, 1))
+        y_train_split_pp = pp.transform(y_train_split[reg].values.reshape(-1, 1))
+        y_val_pp = pp.transform(y_val[reg].values.reshape(-1, 1))
+        y_test_pp = pp.transform(y_test[reg].values.reshape(-1, 1))
 
-asv,chem= data_create('data\\raw\\taxon_data\\taxon_lv7.csv','data\\raw\\chem_data.xlsx')
+        scalers[reg] = pp  # スケーラーを保存
 
-x_train,x_test,y_train,y_test = train_test_split(asv,chem,test_size=0.3)
-print(x_train.dtypes)
+        Y_train_tensor.append(torch.tensor(y_train_split_pp, dtype=torch.float32))
+        Y_val_tensor.append(torch.tensor(y_val_pp, dtype=torch.float32))
+        Y_test_tensor.append(torch.tensor(y_test_pp, dtype=torch.float32))
 
-a,b,c = transform_after_split(x_train,x_test,y_train,y_test,reg_list = ['pH'])
+        #train_set = TensorDataset(X_train_tensor, Y_train_tensor)
+        #val_set = TensorDataset(X_val_tensor, Y_val_tensor)
+        #test_set = TensorDataset(X_test_tensor, Y_test_tensor)
 
-# データを `for` 文で回して表示
-for i, (x, y) in enumerate(a):
-    print(f"データ {i+1}: x = {x}, y = {y}")
+    return X_train_tensor, X_val_tensor, X_test_tensor, Y_train_tensor, Y_val_tensor, Y_test_tensor,scalers
