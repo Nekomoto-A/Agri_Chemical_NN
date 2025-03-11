c8b69e89ca1cd0e1bc390821f9d164209ced26ac
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-03-12 08:31:35 +0900

    model
    kfold edit

--- src/test/test.py
+++ src/test/test.py
@@ -38,4 +38,43 @@ def test_MT(x_te,y_te,model,output_dims,reg_list,scalers):
             mse_scores.append(mse)
     
     #print(r2_scores, mse_scores)
-    return r2_scores, mse_scores
+    return output, true, r2_scores, mse_scores
+
+from src.datasets.dataset import data_create,transform_after_split
+from src.training.train import training_MT
+from src.test.test import test_MT
+from src.models.MT_CNN import MTCNNModel
+
+from sklearn.model_selection import KFold
+import numpy as np
+
+def fold_evaluate(feature_path, target_path, reg_list, exclude_ids,
+                  output_path ='a',k = 5, val_size = 0.2, early_stopping = True, epochs = 10000, lr=0.0001):
+    asv,chem= data_create(feature_path, target_path, reg_list)
+    mask = ~chem['crop-id'].isin(exclude_ids)
+    X, Y = asv[mask], chem[mask]
+
+    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+    for fold, (train_index, test_index) in enumerate(kf.split(X, Y)):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
+
+        X_train_tensor, X_val_tensor, X_test_tensor, Y_train_tensor, Y_val_tensor, Y_test_tensor,scalers = transform_after_split(X_train,X_test,Y_train,Y_test,reg_list = reg_list,
+                                                                                                                         val_size = val_size)
+
+        input_dim = X_train.shape[1]
+        output_dims = np.ones(len(reg_list), dtype="int16")
+
+        model = MTCNNModel(input_dim = input_dim,output_dims = output_dims)
+        # 回帰用の損失関数（MSE）
+        loss_fn = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+
+        epochs = epochs
+        model_trained = training_MT(X_train_tensor,X_val_tensor,Y_train_tensor,Y_val_tensor,model,epochs,loss_fn,optimizer, output_path,output_dim=output_dims,early_stopping = early_stopping)
+
+        outputs, trues, r2_results, mse_results = test_MT(X_test_tensor,Y_test_tensor,model_trained,output_dims,reg_list,scalers)
+
+        # --- 4. 結果を表示 ---
+        for i, (r2, mse) in enumerate(zip(r2_results, mse_results)):
+            print(f"Output {i+1} ({reg_list[i]}): R^2 Score = {r2:.4f}, MSE = {mse:.4f}")
