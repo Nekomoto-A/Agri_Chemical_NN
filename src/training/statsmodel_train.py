6ee266cb15ac6f95aaf32c876bfa6dde9450e813
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-03-15 16:29:27 +0900

    model
    statsmodels edit

---
+++ src/training/statsmodel_train.py
@@ -0,0 +1,25 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def statsmodel_train(X,Y,scalers,reg):
+    models = {}
+    # モデルの定義
+
+    X = X.numpy()
+    Y = Y[0].numpy().reshape(-1, 1)
+
+    models = {
+        "RF": RandomForestRegressor(),
+        "XGB": XGBRegressor(),
+        "SVR": SVR()
+    }
+
+    # モデルの学習
+    for name, model in models.items():
+        model.fit(X, Y)
+    print(models)
+    return models
