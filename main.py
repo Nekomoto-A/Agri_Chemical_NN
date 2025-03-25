48a29c2e37819c9022f9be74e423adcbaab4b342
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-03-26 08:56:29 +0900

    model
    final main

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.test.fold_eval import fold_evaluate
 
-reg_list = ['pH','Available.P']
+#reg_list = ['pH','Available.P']
 #reg_list = ['CEC', 'Exchangeable.Ca', 'Exchangeable.K','Exchangeable.Mg']
 #reg_list = ['CEC', 'Humus']
 #reg_list = ['EC', 'NH4.N','NO3.N','Inorganic.N']
+
+
+reg_list = [['pH','Available.P'],
+['CEC', 'Exchangeable.Ca', 'Exchangeable.K','Exchangeable.Mg'],
+['CEC', 'Humus'],
+['EC', 'NH4.N','NO3.N','Inorganic.N']]
 
 exclude_ids = [
     '041_20_Sait_Carr', '042_20_Sait_Eggp', '043_20_Sait_Carr', '044_20_Sait_Broc',
@@ -12,9 +18,15 @@ exclude_ids = [
     '067_20_Naga_Pump', '331_22_Niig_jpea', '332_22_Niig_jpea'
 ]
 
+#exclude_ids = ['a']
+
 url1 = 'data\\raw\\taxon_data\\taxon_lv6.csv'
 url2 = 'data\\raw\\chem_data.xlsx'
 
-fold_evaluate(feature_path = url1, target_path = url2, reg_list = reg_list, exclude_ids = exclude_ids,
-              epochs = 1, patience = 20)
+epoch = 1000
+
+for reg in reg_list:
+    fold_evaluate(feature_path = url1, target_path = url2, reg_list = reg, exclude_ids = exclude_ids,
+                    epochs = epoch, patience = epoch, lr=0.00001,
+                    output_dir = f'result_{epoch}')
 
