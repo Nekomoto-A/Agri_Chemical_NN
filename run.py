c04ba68720a6d1625cb851ce3308497857136638
Nekomoto-A <s233147@stn.nagaokaut.ac.jp>
2025-04-05 22:56:46 +0900

    l2result add

---
+++ run.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Gitの操作を実行 (例: リポジトリの状態を確認)
+subprocess.run(["git", "status"])
+
+# Pythonスクリプトを実行 (例: 任意のPythonスクリプトを実行)
+subprocess.run(["python", "main.py"])
+
+# さらにGitの操作を実行 (例: コミットを実行)
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "l2result add"])
+subprocess.run(["git", "push","origin","experiment"])
