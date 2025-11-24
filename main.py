ccf825c6a8ba814c9eaea954c593ccabbbd76c80
Asahi Nomura <s233147@stn.nagaokaut.ac.jp>
2025-11-24 15:13:36 +0900

    main
    windows add

--- main.py
+++ main.py
@@ -18,16 +18,27 @@ warnings.filterwarnings('ignore', category=UserWarning)
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 def main():
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    print(f"使用するデバイス: {device}")
+    
     reg_list = config['reg_list']
     if any(isinstance(i, list) for i in reg_list) == False:
         reg_list = [s.replace('.', '_') for s in reg_list]
         #fold_evaluate(reg_list = reg_list)
-        loop_evaluate(reg_list = reg_list)
+
+        loop_evaluate(reg_list = reg_list, output_dir = config['result_dir'], device = device)
     else:
         for reg in reg_list:
             reg = [s.replace('.', '_') for s in reg]
             #fold_evaluate(reg_list = reg)
-            loop_evaluate(reg_list = reg)
+            loop_evaluate(reg_list = reg, output_dir = config['result_dir'], device = device)
 
 if __name__ == '__main__':
     main()
